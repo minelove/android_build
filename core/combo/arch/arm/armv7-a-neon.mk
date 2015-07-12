74e237d8e7432271139fc0d5c8478052e6fc9c77
Steve Kondik <steve@cyngn.com>
2015-07-12 12:14:56 -0700

    Revert "Disable -mcpu=cortex-a15 flag for krait"
     * No issues with this. Revert back to Google's config.
    
    This reverts commit 30579b41d1e943a7740b3dfe979d40f40ba0d8cc.
    
    Change-Id: Ib2953ccc91ce78ef849ddd9aafd6453ea5ddbed4

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -7,26 +7,26 @@ ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
 ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	# NOTE: krait is not a cortex-a15, we set the variant to cortex-a15 so that
-	#       hardware divide operations are generated.
-	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mfloat-abi=softfp
-
-	# Fake an ARM compiler flag as these processors support LPAE which GCC/clang
-	# don't advertise.
-	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1
+	arch_variant_cflags := -mcpu=cortex-a15
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+	arch_variant_cflags := -mcpu=cortex-a9
 else
 ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp
-	arch_variant_ldflags := -Wl,--fix-cortex-a8 
+	arch_variant_cflags := -mcpu=cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon -mfloat-abi=softfp
+	arch_variant_cflags := -mcpu=cortex-a7
 else
-	arch_variant_cflags := -march=armv7-a -mfpu=neon -mfloat-abi=softfp
+	arch_variant_cflags := -march=armv7-a
 endif
 endif
 endif
 endif
+
+arch_variant_cflags += \
+    -mfloat-abi=softfp \
+    -mfpu=neon
+
+arch_variant_ldflags := \
+	-Wl,--fix-cortex-a8
